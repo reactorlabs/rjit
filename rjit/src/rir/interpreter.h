f3525f1b1bb4fdcb27590d0b794686da6649d687
jan vitek <janvite@yahoo.com>
2016-06-21 11:45:06 -0400

    ok

--- rjit/src/rir/interpreter.h
+++ rjit/src/rir/interpreter.h
@@ -395,21 +395,22 @@ INLINE size_t addSource(SEXP value) {
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 // bytecode basics
 
+    //                 immediate pop   push popi pushi
 
-    def_instr(invalid_,   0,    0,   0,   0) // invalid operation
-    def_instr(push_,      0,    0,   0,   0) // push a constant to the stack
-    def_instr(ldfun_,     0,    0,   0,   0) // load function from env
-    def_instr(ldvar_,     0,    0,   0,   0) // load variable from env
-    def_instr(call_,      0,    0,   0,   0) // call fun
-    def_instr(promise_,   0,    0,   0,   0) // create promise
-    def_instr(close_,     0,    0,   0,   0) // create closure
-    def_instr(ret_,       0,    0,   0,   0) // return
-    def_instr(force_,     0,    0,   0,   0) // eval promise
-    def_instr(pop_,       0,    0,   0,   0) // pop tos
-    def_instr(pusharg_,   0,    0,   0,   0) // push argument to stack
-    def_instr(asast_,     0,    0,   0,   0) // pop promise, push ast
-    def_instr(stvar_,     0,    0,   0,   0) // ??
-    def_instr(numargi_,   0,    0,   0,   0) // ??
+    def_instr(invalid_,   0,    0,    0,   0,   0) // invalid operation
+    def_instr(push_,      1,    0,    1,   0,   0) // push a constant to the stack
+    def_instr(ldfun_,     1,    0,    1,   0,   0) // load function from env
+    def_instr(ldvar_,     1,    0,    1,   0,   0) // load variable from env
+    def_instr(call_,      ?,    0,   0,   0) // call fun
+    def_instr(promise_,   1,    0,   1,   0) // create promise
+    def_instr(close_,     1,    0,   0,   0) // create closure
+    def_instr(ret_,       0,    1,   0,   0) // return
+    def_instr(force_,     0,    1,   1,   0) // eval promise
+    def_instr(pop_,       0,    1,   0,   0) // pop tos
+    def_instr(pusharg_,   1,    0,   0,   0) // push argument to stack
+    def_instr(asast_,     0,    1,   1,   0) // pop promise, push ast
+    def_instr(stvar_,     0,    2,   1,   0) // ??
+    def_instr(numargi_,   0,    0,   1,   0) // ??
     def_instr(asbool_,    0,    0,   0,   0) // convert ?? on tos to bool
     def_instr(condjmp_,   0,    0,   0,   0) // jump … if true
     def_instr(jmp_,       0,    0,   0,   0) // unconditional jump
@@ -425,7 +426,7 @@ INLINE size_t addSource(SEXP value) {
     def_instr(isfun_,     0,    0,   0,   0) // check tos is a function
     def_instr(end_,       0,    0,   0,   0)
 
-    
+
 
   // enums in C are not namespaces so I am using OP_ to disambiguate
 typedef enum {
